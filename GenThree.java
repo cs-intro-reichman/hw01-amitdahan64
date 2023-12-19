2f73cafcf9926f63d2b7d9dea609e5b71e3acee3
amitdahan64 <amit.dahan01@post.runi.ac.il>
2023-12-19 22:38:09 +0200

    HW01 submission

--- GenThree.java
+++ GenThree.java
@@ -3,7 +3,29 @@
  * prints them, and then prints the minimal number that was generated.
  */
 public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+    public static void main(String[] args) {
+
+        /* Checking and storing min, max and range values
+         creating numbers array for later use. */
+        int min = Math.min(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        int max = Math.max(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        int range = max-min +1;
+        int[] numbers = new int[3];
+        
+        // Printing 3 random numbers and storing them in numbers array
+        for(int i = 0; i < 3; i++) {
+            int random = (int)((Math.random() * range ) + min);
+            System.out.println(random);
+            numbers[i] = random;
+        }
+
+        // Finding the lowest number and printing it
+        int lowest = Math.min(numbers[0], numbers[1]);
+        lowest = Math.min(lowest, numbers[2]);
+        System.out.println("The minimal generated number was " + lowest);
+        
+
+
+
+    }
 }
--- Triangle.java
+++ Triangle.java
@@ -4,7 +4,28 @@
  * Write a program that tests if three given integers form a triangle.
  */ 
 public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+    public static void main(String[] args) {
+
+        //Storing the triangle's sides values
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        boolean isTriangle = false;
+
+        /* Checking if the sides can form a triangle according
+        to the Triangle Inequality Theorem */
+        if(a+b>c) {
+
+            if(a+c>b) {
+
+                if(c+b>a) {
+
+                    isTriangle = true;
+                }
+            }
+        }
+
+        System.out.println(a + ", " + b + ", " + c + ": " + isTriangle);
+    }
+    
 }
